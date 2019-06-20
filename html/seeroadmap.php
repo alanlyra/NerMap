4b00cc462c4bc9f791566bd7636545f5bc46e251
Alan <alanlyra7@gmail.com>
2019-06-20 03:21:50 -0300

    Melhoria no roadmap

--- html/seeroadmap.php
+++ html/seeroadmap.php
@@ -43,118 +43,117 @@ saveCurrentURL();
                   <h6 class="m-0 font-weight-bold text-primary">Roadmap para a área de...</h6>
                 </div>
 
-           <div class="container">
+           <div class="container" style="background: #333 !important; max-height: 55vh; overflow: auto;">
 
-  
-        <ul class="timeline">
-          <li><div class="tldate">2020</div></li>
-          
-          <li>
-            <div class="tl-circ"></div>
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>Surprising Headline Right Here</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3 hours ago</small></p>
-              </div>
-              <div class="tl-body">
-                <p>Lorem Ipsum and such.</p>
-              </div>
-            </div>
-          </li>
-          
-          <li class="timeline-inverted">
-            <div class="tl-circ"></div>
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>Breaking into Spring!</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 4/07/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>Hope the weather gets a bit nicer...</p>
-                  
-                <p>Y'know, with more sunlight.</p>
-              </div>
-            </div>
-          </li>
-          
-          <li><div class="tldate">2025</div></li>
-          
-          <li>
-            <div class="tl-circ"></div>
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>New Apple Device Release Date</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/22/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>In memory of Steve Jobs.</p>
-              </div>
-            </div>
-          </li>
-          <li class="timeline-inverted">
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>No icon here</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/16/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>Here you'll find some beautiful photography for your viewing pleasure, courtesy of <a href="http://lorempixel.com/">lorempixel</a>.</p>
-                
-                <p><img src="http://lorempixel.com/600/300/nightlife/" alt="lorem pixel"></p>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="tl-circ"></div>
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>Some Important Date!</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/03/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>Write up a quick summary of the event.</p>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="timeline-panel noarrow">
-              <div class="tl-heading">
-                <h4>Secondary Timeline Box</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/01/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>This might be a follow-up post with related info. Maybe we include some extra links, tweets, user comments, photos, etc.</p>
-              </div>
-            </div>
-          </li>
-          
-          <li><div class="tldate">2030</div></li>
-          
-          <li class="timeline-inverted">
-            <div class="tl-circ"></div>
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>The Winter Months</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 02/23/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>Gee time really flies when you're having fun.</p>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="tl-circ"></div>
-            <div class="timeline-panel">
-              <div class="tl-heading">
-                <h4>Yeah we're pretty much done here</h4>
-                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 02/11/2014</small></p>
-              </div>
-              <div class="tl-body">
-                <p>Wasn't this fun though?</p>
-              </div>
-            </div>
-          </li>
-        </ul>
+            <ul class="timeline">
+              <li><div class="tldate">2020</div></li>
+              
+              <li>
+                <div class="tl-circ"></div>
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>Surprising Headline Right Here</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3 hours ago</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>Lorem Ipsum and such.</p>
+                  </div>
+                </div>
+              </li>
+              
+              <li class="timeline-inverted">
+                <div class="tl-circ"></div>
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>Breaking into Spring!</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 4/07/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>Hope the weather gets a bit nicer...</p>
+                      
+                    <p>Y'know, with more sunlight.</p>
+                  </div>
+                </div>
+              </li>
+              
+              <li><div class="tldate">2025</div></li>
+              
+              <li>
+                <div class="tl-circ"></div>
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>New Apple Device Release Date</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/22/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>In memory of Steve Jobs.</p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>No icon here</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/16/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>Here you'll find some beautiful photography for your viewing pleasure, courtesy of <a href="http://lorempixel.com/">lorempixel</a>.</p>
+                    
+                    <p><img src="http://lorempixel.com/600/300/nightlife/" alt="lorem pixel"></p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="tl-circ"></div>
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>Some Important Date!</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/03/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>Write up a quick summary of the event.</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="timeline-panel noarrow">
+                  <div class="tl-heading">
+                    <h4>Secondary Timeline Box</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3/01/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>This might be a follow-up post with related info. Maybe we include some extra links, tweets, user comments, photos, etc.</p>
+                  </div>
+                </div>
+              </li>
+              
+              <li><div class="tldate">2030</div></li>
+              
+              <li class="timeline-inverted">
+                <div class="tl-circ"></div>
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>The Winter Months</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 02/23/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>Gee time really flies when you're having fun.</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="tl-circ"></div>
+                <div class="timeline-panel">
+                  <div class="tl-heading">
+                    <h4>Yeah we're pretty much done here</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 02/11/2014</small></p>
+                  </div>
+                  <div class="tl-body">
+                    <p>Wasn't this fun though?</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
         </div>
         <input type="text" id="foo" name="foo" class="form-control bg-light border-0 small" placeholder="" aria-label="Search" aria-describedby="basic-addon2">
         <!-- /.container-fluid -->
